a288cb51b1affa4b5a14c4eb8b5c858ff52c4e1b
Curtis Alexander <calex@calex.org>
2020-01-30 20:42:55 -0600

    Swap out code for some developed for other project

--- __app__/WriteShortUrl/__init__.py
+++ __app__/WriteShortUrl/__init__.py
@@ -1,24 +1,67 @@
+import json
 import logging
+import os
 
+from azure.cosmosdb.table.tableservice import TableService
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    def get_param(req: func.HttpRequest, param: str) -> str:
+        param_value = req.params.get(param)
+        if not param_value:
+            try:
+                req_body = req.get_json()
+            except ValueError:
+                pass
+            else:
+                param_value = req_body.get(param)
+        return param_value
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
+    path = get_param(req, "path")
+    longUrl = get_param(req, "longurl")
+
+    logging.info(f"Parsed the path as {path} and the longUrl as {longUrl}.")
+
+    if path and longUrl:
+        rowKey = path
+        baseUrl = os.environ["BASE_URL"]
+        shortUrl = f"{baseUrl}/{path}"
+
+        data = {
+            "PartitionKey": "links",
+            "RowKey": rowKey,
+            "ShortUrl": shortUrl,
+            "LongUrl": longUrl,
+        }
+
+        # connect to table
+        storage_account_name = os.environ["STORAGE_ACCOUNT_NAME"]
+        storage_account_key = os.environ["STORAGE_ACCOUNT_KEY"]
+
+        table_service = TableService(
+            account_name=storage_account_name, account_key=storage_account_key
+        )
+
+        # write to table
+        # tbl.set(json.dumps(data))
+        table_service.insert_or_replace_entity("links", data)
+
+        resp = {
+            "Status": "Ok",
+            "Message": f"Wrote the following to Azure Table Storage: PartitionKey={data['PartitionKey']}, RowKey={data['RowKey']}, ShortUrl={data['ShortUrl']}, LongUrl={data['LongUrl']}",
+        }
+        resp_json = json.dumps(resp)
+
+        return func.HttpResponse(body=resp_json, mimetype="application/json")
     else:
+        resp = {
+            "Status": "Error",
+            "Message": "Did not write any links to Azure Table Storage",
+        }
+        resp_json = json.dumps(resp)
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+            body=resp_json, mimetype="application/json", status_code=400
         )
